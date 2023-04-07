c87e66f2f3366fe1715e1870ddcffff756b16f38
lvokic <2792779840@qq.com>
2023-04-08 00:11:21 +0800

    a better look of front

--- wj-vue/src/main.js
+++ wj-vue/src/main.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.prototype.$axios = axios
 axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
-
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render:h=>h(App),
   router,
   components: { App },
   template: '<App/>'
