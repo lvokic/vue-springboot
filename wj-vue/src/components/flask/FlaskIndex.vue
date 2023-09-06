d3f3ae8d269afc5d9d475972f1737fb54a0e267f
lvokic <2792779840@qq.com>
2023-09-06 17:56:46 +0800

    process on flask

--- wj-vue/src/components/flask/FlaskIndex.vue
+++ wj-vue/src/components/flask/FlaskIndex.vue
@@ -1,33 +1,35 @@
 <template>
-    <el-container>
-      <el-aside style="width: 100px;margin-top: 20px;">
-        <switch></switch>
-      </el-aside>
-      <el-main> 
-        <flask class="flask-area"></flask>
-      </el-main>
-    </el-container>
-  </template>
- 
-  <script>
-  import Flask from './Flask.vue';
-  export default {
-    beforeRouteEnter(to, from, next) {
-      // 添加背景色 margin:0;padding:0是为了解决vue四周有白边的问题
-      document.querySelector('body').setAttribute('style', 'margin:0;padding:0')
-      next()
-    },
-    name: 'AppFlask',
-    components: { Flask }
-  }
-  </script>
-    
-  <style scoped>
-  .flask-area {
-    width: 990px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  </style>
-    
-    
+  <el-container>
+    <el-main>
+      <div class="flask-area">
+        <flask></flask>
+      </div>
+      <div class="upload-area">
+        <upload></upload>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import Flask from './Flask.vue';
+import Upload from './Upload.vue';
+
+export default {
+  beforeRouteEnter(to, from, next) {
+    // 添加背景色 margin:0;padding:0是为了解决vue四周有白边的问题
+    document.querySelector('body').setAttribute('style', 'margin:0;padding:0');
+    next();
+  },
+  name: 'AppFlask',
+  components: { Flask, Upload },
+};
+</script>
+
+<style scoped>
+.flask-area,
+.upload-area {
+  width: 700px;
+  margin: 20px auto; /* 居中显示，上下外边距为 20px */
+}
+</style>
---
+++ wj-vue/src/components/flask/Upload.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="upload">
+    <el-upload
+      ref="uploadRef"
+      class="upload-demo"
+      drag
+      :action="uploadUrl"
+      :multiple="true"
+      :before-upload="beforeUpload"
+      :on-success="handleSuccess"
+      :on-error="handleError"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件,且不超过500kb</div>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Upload',
+  data() {
+    return {
+      uploadUrl: 'http://127.0.0.1:5000/api-flask/upload/',
+    };
+  },
+  methods: {
+    handleSuccess(response, file) {
+      // 处理成功上传后的响应
+      console.log(response);
+      // 可以在这里更新Vue组件中的数据或者显示上传成功信息
+    },
+    handleError(error, file) {
+      // 处理上传失败的情况
+      console.error(error);
+      // 可以在这里显示上传失败信息
+    },
+    beforeUpload(file) {
+      // 可以在这里添加一些上传前的逻辑，例如文件类型、大小等的验证
+      console.log('Before Upload:', file);
+      return true; // 返回 true 才会执行上传操作
+    },
+    triggerUpload() {
+      // 手动触发文件上传
+      this.$refs.uploadRef.submit();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.upload {
+  margin-top: 10px;
+}
+</style>
